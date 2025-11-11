8bcd6b75bd7f58f91df3f7c310e8c05fe55a4095
dobodu <ludovic.boldu@sfr.fr>
2025-11-11 18:49:36 +0100

    Add files via upload
    Added build files for ESP32

---
+++ ESP32_GENERIC_S3/mpconfigvariant_FLASH_16M_SPIRAM_OCT.cmake
@@ -0,0 +1,43 @@
+
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
+    boards/sdkconfig.usb
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram_sx
+    boards/ESP32_GENERIC_S3/sdkconfig.board
+)
+
+if(MICROPY_BOARD_VARIANT STREQUAL "SPIRAM_OCT")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/sdkconfig.240mhz
+        boards/sdkconfig.spiram_oct
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="Generic ESP32S3 module with Octal-SPIRAM"
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_4M")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/ESP32_GENERIC_S3/sdkconfig.flash_4m
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_16M_SPIRAM_OCT")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/sdkconfig.240mhz
+        boards/sdkconfig.spiram_oct
+        boards/ESP32_GENERIC_S3/sdkconfig.flash_16m
+    )
+
+     list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="ESP32S3 microcontroller with 16MB of Flash & Octal-SPIRAM"
+    )
+endif()
