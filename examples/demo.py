8e4e12862cd50f3de676bb6fdfda91006dbb2ea5
dobodu <ludovic.boldu@sfr.fr>
2025-12-06 19:15:41 +0100

    Update demo.py
    Add TTF font in demo

--- examples/demo.py
+++ examples/demo.py
@@ -17,25 +17,30 @@ elif BOARD == "WS_241_AMOLED" :
 
 POLYGON = [(50,-25),(25,-25),(25,-50),(-25,-50),(-25,-25),(-50,-25),(-50,25),(-25,25),(-25,50),(25,50),(25,25),(50,25),(50,-25)]
 
-#font = display.ttf_load_font("/fonts/test.ttf")
+display.ttf_load_font("/fonts/test.ttf")
+display.ttf_init_font()
+font_scale = 24
+display.ttf_scale_font(font_scale)
 
 def main():
     display.reset()
     display.init()
     display.rotation(3)
     display.brightness(0)
-    display.jpg("/bmp/smiley_small.jpg",80,0)
+    display.jpg("/bmp/smiley_small.jpg",0,0)
     for i in range(255):
         display.brightness(i)
-        utime.sleep(0.02)
-    utime.sleep(1)
+        utime.sleep(0.01)
+    utime.sleep(0.5)
     display.fill(amoled.BLACK)
-    utime.sleep(1)
+    utime.sleep(0.5)
     
     text = "Hello!"
-    text_length = display.write_len(font,text)
-    text_height = font.HEIGHT
-
+    write_length = display.write_len(font,text)
+    write_height = font.HEIGHT
+    ttf_write_length = display.ttf_len(text)
+    ttf_write_height = font_scale
+    
     while True:
 
         for rotation in range(4):
@@ -43,8 +48,10 @@ def main():
             display.fill(amoled.BLACK)
             col_max = display.width()
             row_max = display.height()
-            text_xmax = col_max - text_length
-            text_ymax = row_max - text_height
+            write_xmax = col_max - write_length
+            write_ymax = row_max - write_height
+            ttf_write_xmax = col_max - ttf_write_length
+            ttf_write_ymax = row_max - ttf_write_height
             
             filled = random.randint(0,1)
             kind = random.randint(0,5)
@@ -68,24 +75,28 @@ def main():
                         random.getrandbits(8))
                 
                 if kind == 0 :
+                    kind_name = "Circl"
                     if filled :
                         display.fill_circle(xpos,ypos,radius, color)
                     else :
                         display.circle(xpos, ypos, radius, color)
                 
                 if kind == 1 :
+                    kind_name = "Recta"
                     if filled :
                         display.fill_rect(xpos,ypos,length, height, color)
                     else :
                         display.rect(xpos,ypos,length, height, color)
                 
                 if kind == 2 :
+                    kind_name = "BRect"
                     if filled :
                         display.fill_bubble_rect(xpos,ypos,length, height, color)
                     else :
                         display.bubble_rect(xpos,ypos,length, height, color)
                         
                 if kind == 3 :
+                    kind_name = "Trian"
                     if filled :
                         display.fill_trian(random.randint(0, col_max), random.randint(0, row_max),
                                        random.randint(0, col_max), random.randint(0, row_max),
@@ -96,20 +107,30 @@ def main():
                                        random.randint(0, col_max), random.randint(0, row_max), color)
                         
                 if kind == 4 :
+                    kind_name = "BITfon"
                     if filled :
-                        display.write(font,text, random.randint(10, text_xmax-10), random.randint(10, text_ymax-10), color,color2)
+                        display.write(font,text, random.randint(10, write_xmax-10), random.randint(10, write_ymax-10), color,color2)
                     else :
-                        display.write(font,text, random.randint(10, text_xmax-10), random.randint(10, text_ymax-10), color)
+                        display.write(font,text, random.randint(10, write_xmax-10), random.randint(10, write_ymax-10), color)
                         
                 if kind == 5 :
+                    kind_name = "Poly"
                     if filled :
                         display.fill_polygon(POLYGON, random.randint(55, col_max-55), random.randint(55, row_max-55) , color, angle)
                     else :
                         display.polygon(POLYGON, random.randint(55, col_max-55), random.randint(55, row_max-55) , color, angle)
-            
+                        
+                if kind == 6 :
+                    kind_name = "TTFfon"
+                    if filled :
+                        display.ttf_draw(text, random.randint(10, ttf_write_xmax-10), random.randint(10+ttf_write_height, ttf_write_ymax-10), color,color2)
+                    else :
+                        display.ttf_draw(text, random.randint(10, ttf_write_xmax-10), random.randint(10+ttf_write_height, ttf_write_ymax-10), color)
+
+
             end_time = utime.ticks_ms()
             fps = 1000*128/(end_time - start_time)
-            fps_txt = "Rot {:.0f} - {:.0f}/s".format(rotation,fps)
+            fps_txt = "Rot {:.0f}-{}-{:.0f}/s".format(rotation,kind_name,fps)
             display.write(font,fps_txt, 0, 0, color)
             print(rotation, kind, filled, fps) 
             
